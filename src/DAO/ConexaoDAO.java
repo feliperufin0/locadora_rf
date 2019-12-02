b83c536d0ac46a1314638b89b704f3fab7e0f554
pedro lucca <pedro lucca@meister>
2019-12-02 14:21:58 -0300

    ConxexaoDAO

--- src/DAO/ConexaoDAO.java
+++ src/DAO/ConexaoDAO.java
@@ -5,10 +5,32 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Felipe Rufino
  */
 public class ConexaoDAO {
-    
+    public static Connection  AbrirConexao;
+        Connection con = null;
+         try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jbdc:mysql://localhost/locadora";
+            con = DriverManager.getConnection(url, root,"");
+        }catch (Exception e){
+            JOptionPane.ShowMessageDialog(null, "Erro na conexão com o banco",
+                    "Video Locadora", JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+        
+        }
+            return con;
+}
+        public static void FecharConexao(Connection con){
+            try{
+                con.close();
+        }catch (Exception e){
+        System.out.println(e.getMessage());
+    }
 }
