af6805b974b083b217afdf76e436483687a09689
pedro lucca <pedro lucca@meister>
2019-12-10 12:57:54 -0300

    AlterarCliente

--- src/DAO/ClienteDAO.java
+++ src/DAO/ClienteDAO.java
@@ -139,6 +139,37 @@ public class ClienteDAO extends ExecuteSQL{
            return null;
        }
    }
+   
+   public String Alterar_Cliente(Cliente a){
+       String sql = "update cliente set nome = ? ,data_nasc = ? ,rg = ?"
+               + ",cpf = ? ,email = ? ,telefone = ? ,bairrro = ? ,rua = ?"
+               +",numero = ?,cep = ? where idcliente = ?";
+            try{
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ps.setString(1, a.getNome());
+                ps.setString(2, a.getNascimento());
+                ps.setString(3, a.getRG());
+                ps.setString(4, a.getCPF());
+                ps.setString(5, a.getEmail());
+                ps.setString(6, a.getTelefone());
+                ps.setString(7, a.getBairro());
+                ps.setString(8, a.getRua());
+                ps.setInt(9, a.getNumero());
+                ps.setString(10, a.getCEP());
+                ps.setInt(11, a.getCodigo());
+                if(ps.executeUpdate() > 0){
+                    return "Atualizado com sucesso.";
+                }else{
+                    return "Erro ao Atualizar";
+                }
+            }catch (SQLException e){
+                return e.getMessage();
+            }
+   }
+            
+            
+   
+   
 }
            
        
